206042125b2c540cc2803fd67f40c205210aa62e
yamazoon.com <yamamoto@serverworks.co.jp>
2022-02-04 14:47:12 +0900

    1st commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+ECSを作成する予定
+前提：Terraform , AWSアカウント
---
+++ alb.tf
@@ -0,0 +1,46 @@
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "yamamoto-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
+  security_groups = [aws_security_group.lb_example.id]
+
+  /*
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "tg1-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    },
+    {
+      name_prefix      = "tg2-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+  */
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
+}
